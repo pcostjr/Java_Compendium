0a1511deacecc70693cd5884f4d9c2d1858837a9
pcostjr <pcostjr@proton.me>
2025-09-16 14:03:59 -0400

    Added example work in sandbox.

--- Sandbox/src/Main.java
+++ Sandbox/src/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Cube c1 = new Cube("pink","metal", "underwater", 690);
-        Cube c2 = new Cube("blue", "foam", "logo", 3);
-        Cube c3 = new Cube();
+        // What is printed as a result of executing the code segment?
+        int a = 5;
+        float b = 8;
+        float c = 3;
+        System.out.println(a + b / c * 2);
 
 
-        System.out.println("Cube c1: " + c1.getSide());
-        System.out.println("Cube c1 surface area: " + c1.surfaceArea());
-        System.out.println("Cube c1 volume: " + c1.volume());
+        // Hypothetical code segment problem
+//        int onesDigit = num % 10;
+//        int tensDigit = num / 10;
 
-        System.out.println(c1);
-        System.out.println(c2);
-        System.out.println(c3);
-
-
-
+        // variable reassignment & pass-by-value
+//        int x = 5;
+//        int y = 7;
+//        int temp = x;
+//        x = y;
+//        y = temp;
     }
 }
