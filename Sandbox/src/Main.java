ff9161a00c9555c39611269777013b79ed8f2903
pcostjr <pcostjr@proton.me>
2025-09-26 12:05:58 -0400

    final submission

--- Sandbox/src/Main.java
+++ Sandbox/src/Main.java
@@ -1,11 +1,32 @@
+import java.lang.Math;
+
 public class Main {
     public static void main(String[] args) {
         // What is printed as a result of executing the code segment?
-        int a = 5;
-        float b = 8;
-        float c = 3;
-        System.out.println(a + b / c * 2);
+//        int x = 5;
+//        int y = 4;
+//        int sum = x + y;
+//        double average = (double) (sum / 2);
+//        System.out.println(average);
+//        double average2 = (double) sum / 2;
+//
+//        System.out.println(average2);
 
+        double val = -2.5;
+
+        for(double i = -2; i <= 2; i += 0.1)
+        {
+            int apRound = (int) (i + 0.5);
+            long mathRound = Math.round(i);
+            System.out.printf("Result from rounding function for i = %f: AP: %d | Math: %d\n", i, apRound, mathRound);
+        }
+
+//        {
+//            ashfaslkfjalksdjlkf;ajsdlkfjalks;djflk;adsjlkf;ajsdlkf;jaslkdjflkadsjflk;as
+//                lkj;asjdflk;ajsdlk;jfalks;djflk;ajdslkfajsdlkjflk;asdjf
+//                als;djflkajslk;fjalks;jflk;asjlk;fjaslkd;jfa;s
+//
+//        }
 
         // Hypothetical code segment problem
 //        int onesDigit = num % 10;
